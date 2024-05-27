87b9dabebd206d6395cc96eb29c7c91920e1b557
edu13633 <39829003+edu13633@users.noreply.github.com>
2024-05-27 20:28:10 +0200

    Add files via upload

--- ModeloERP/ModeloERP/FormCrearFactura.designer.vb
+++ ModeloERP/ModeloERP/FormCrearFactura.designer.vb
@@ -34,6 +34,10 @@ Partial Class FormCrearFactura
         Me.Label2 = New System.Windows.Forms.Label()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'ListBox1
@@ -136,11 +140,40 @@ Partial Class FormCrearFactura
         Me.Label1.TabIndex = 45
         Me.Label1.Text = "Proveedor"
         '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(312, 12)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(285, 222)
+        Me.DataGridView1.TabIndex = 58
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(370, 262)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(34, 13)
+        Me.Label4.TabIndex = 59
+        Me.Label4.Text = "Total:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(460, 262)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(64, 13)
+        Me.Label5.TabIndex = 60
+        Me.Label5.Text = "Precio Total"
+        '
         'FormCrearFactura
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(321, 348)
+        Me.ClientSize = New System.Drawing.Size(638, 348)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.DataGridView1)
         Me.Controls.Add(Me.ListBox1)
         Me.Controls.Add(Me.TextBox6)
         Me.Controls.Add(Me.Label7)
@@ -155,6 +188,7 @@ Partial Class FormCrearFactura
         Me.Controls.Add(Me.Label1)
         Me.Name = "FormCrearFactura"
         Me.Text = "FormCrearFactura"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -172,4 +206,7 @@ Partial Class FormCrearFactura
     Friend WithEvents Label2 As Label
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents Label1 As Label
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
 End Class
--- ModeloERP/ModeloERP/FormCrearPedido.designer.vb
+++ ModeloERP/ModeloERP/FormCrearPedido.designer.vb
@@ -34,6 +34,10 @@ Partial Class FormCrearPedido
         Me.Label7 = New System.Windows.Forms.Label()
         Me.TextBox6 = New System.Windows.Forms.TextBox()
         Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'TextBox3
@@ -136,11 +140,40 @@ Partial Class FormCrearPedido
         Me.ListBox1.Size = New System.Drawing.Size(222, 69)
         Me.ListBox1.TabIndex = 44
         '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(463, 262)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(64, 13)
+        Me.Label5.TabIndex = 63
+        Me.Label5.Text = "Precio Total"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(373, 262)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(34, 13)
+        Me.Label4.TabIndex = 62
+        Me.Label4.Text = "Total:"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(315, 12)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(285, 222)
+        Me.DataGridView1.TabIndex = 61
+        '
         'FormCrearPedido
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(287, 349)
+        Me.ClientSize = New System.Drawing.Size(655, 349)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.DataGridView1)
         Me.Controls.Add(Me.ListBox1)
         Me.Controls.Add(Me.TextBox6)
         Me.Controls.Add(Me.Label7)
@@ -155,6 +188,7 @@ Partial Class FormCrearPedido
         Me.Controls.Add(Me.Label1)
         Me.Name = "FormCrearPedido"
         Me.Text = "FormCrearPedido"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -171,4 +205,7 @@ Partial Class FormCrearPedido
     Friend WithEvents Label7 As Label
     Friend WithEvents TextBox6 As TextBox
     Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents DataGridView1 As DataGridView
 End Class
